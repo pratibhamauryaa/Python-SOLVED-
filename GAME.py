5aac6a93c17e0580e310ddfd20ad71540b563266
pratibhamauryaa <69991382+pratibhamauryaa@users.noreply.github.com>
2022-03-10 06:15:55 +0530

    Create GAME.py

---
+++ GAME.py
@@ -0,0 +1,12 @@
+def display_function(row1,row2,row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+row1 = [' ',' ',' ']
+row2 = [' ',' ',' ']
+row3 = [' ',' ',' ']
+
+print(display_function(row1,row2,row3))
+
+  
