98a3c6d4d12e40d9586167c6a89813013ec604d5
pratibhamauryaa <69991382+pratibhamauryaa@users.noreply.github.com>
2022-03-10 05:53:12 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,10 +1,1 @@
-def balckjack(a,b,c):
-    # CHECK IF SUM<=21
-    sum = a+b+c   # could hav eused build in sum func-- sum([a,b,c])
-    if sum <=21:
-        return sum
-    # CHECK IS SUM >21 AMD 11 IS THERE?
-    elif 11 in [a,b,c] and sum -10 <= 21:  # ??????????
-        return sum - 10
-    else:
-        return "bust"
+
