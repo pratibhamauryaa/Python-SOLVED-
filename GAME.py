85f45e001e681b51a7cf7c2c963558aaba638cf9
pratibhamauryaa <69991382+pratibhamauryaa@users.noreply.github.com>
2022-03-11 03:03:49 +0530

    Update GAME.py

--- GAME.py
+++ GAME.py
@@ -1,3 +1,5 @@
+#  making the display of our hash grid.
+
 def display_function(row1,row2,row3):
     print(row1)
     print(row2)
@@ -5,8 +7,10 @@ def display_function(row1,row2,row3):
 
 row1 = [' ',' ',' ']
 row2 = [' ',' ',' ']
-row3 = [' ',' ',' ']
+row3 = [' ',' ',' ']      # the index will decide where we ll be putting X and Os
 
 print(display_function(row1,row2,row3))
+
+# Taking the "valid" input. 
 
   
